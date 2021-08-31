c48483569dd30d71ba1e1e716e501e2ba15bb1c6
petumazo <davidalvarezmazzo@gmail.com>
2021-08-31 14:29:37 -0500

    Add task 1 tests.

--- spec/taks1.test.js
+++ spec/taks1.test.js
@@ -1,8 +1,26 @@
 const stringLength = require('../src/task1.js');
 
-test('Return the length of "tests"', () => {
-    expect(stringLength("test")).toBe(4);
+test('Should return the correct lenght', () => {
+    string = 'test';
+
+    testFunction = stringLength(string);
+
+    expect(testFunction).toBe(4);
 });
-test('Return the length of "task1"', () => {
-    expect(stringLength("task1")).toBe(5);
+
+
+test('Should return error if string is empty', () => {
+    string = '';
+
+    testFunction = () => stringLength(string);
+
+    expect(testFunction).toThrow('String should at least 1 character long and not longer than 10 characters.');
+});
+
+test('Return error if string is larger than 10', () => {
+    string = 'Test for check if error is showed when larger than 10';
+
+    testFunction = () => stringLength(string);
+
+    expect(testFunction).toThrow('String should at least 1 character long and not longer than 10 characters.');
 });
--- src/task1.js
+++ src/task1.js
@@ -1,3 +1,6 @@
-let stringLength = string => string.length;
+function stringLength(string) {
+    if (string.length === 0 || string.length > 10) throw new Error('String should at least 1 character long and not longer than 10 characters.');
+    return string.length;
+}
 
 module.exports = stringLength;
