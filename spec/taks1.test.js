1d4ea932164afa9ba3700f5243405fe2555b7ca0
petumazo <davidalvarezmazzo@gmail.com>
2021-08-31 14:32:50 -0500

    Update syntax to ES6.

--- spec/taks1.test.js
+++ spec/taks1.test.js
@@ -17,7 +17,7 @@ test('Should return error if string is empty', () => {
     expect(testFunction).toThrow('String should at least 1 character long and not longer than 10 characters.');
 });
 
-test('Return error if string is larger than 10', () => {
+test('Should return error if string is larger than 10', () => {
     string = 'Test for check if error is showed when larger than 10';
 
     testFunction = () => stringLength(string);
--- src/task1.js
+++ src/task1.js
@@ -1,4 +1,4 @@
-function stringLength(string) {
+const stringLength = (string) => {
     if (string.length === 0 || string.length > 10) throw new Error('String should at least 1 character long and not longer than 10 characters.');
     return string.length;
 }
